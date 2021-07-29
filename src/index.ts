4ffd0d2d35acdfbd7b9a1f050bda2fd4f74dcb09
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-07-29 14:25:47 +0200

    export json

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@
  */
 
 export * from "./array";
+export * from "./json";
 export * from "./primitives";
 export * from "./record";
 export * from "./type";
 
 // TODO: datetime (requires json conversion!)
 // TODO: enum
-// TODO: json (value, object, array)
+

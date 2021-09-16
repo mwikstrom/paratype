6807dc60ba3b9e142e4e45689c0bf478d56dc36c
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-09-16 08:44:08 +0200

    enum type values are readonly

--- src/enum.ts
+++ src/enum.ts
@@ -7,7 +7,7 @@ import { Type } from "./type";
  * @param values - Allowed values of the new enum type
  * @public
  **/
-export function enumType<V extends string>(values: V[]): Type<V> {
+export function enumType<V extends string>(values: readonly V[]): Type<V> {
     const error: Type<string>["error"] = (value, path) => {
         if (typeof value === "string" && values.includes(value as V)) {
             return void(0);
